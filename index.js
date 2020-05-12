910bbe1b79b9a91e35cf79a5660faec0dee4814e
ahmedzakariaghanm <ahmedzakariaghanm@gmail.com>
2020-05-12 20:11:47 +0200

    Edit resulte for registerd user

--- index.js
+++ index.js
@@ -19,15 +19,11 @@ app.use(bodyParser.json())
 var server = require('https').createServer(options, app);
 app.post('/login-with-facebook',async (req,res)=>{
     const {accessToken, userID}=req.body;
-    // console.log(req.body)
     const response = await fetch(`https://graph.facebook.com/v1.0/me?access_token=${accessToken}&method=get&pretty=0&sdk=joey&suppress_http_code=1`)
     const json = await response.json()
-
     if(json.id === userID){
        const resp = await user.findOne({facebookID:userID});
        if(resp){
-        //  res.status(201).send(JSON.stringify({'msg':"You are logged in "}, null, 4));
-
            return res.json({status:'ok',data:resp})
        }
        else {
@@ -37,8 +33,7 @@ app.post('/login-with-facebook',async (req,res)=>{
                accessToken
            })
            await person.save()
-           res.json({status:'ok',data:person})
-
+           return res.json({status:'ok',data:person})
        }
     }
     else{
--- src/script.js
+++ src/script.js
@@ -1,15 +1,8 @@
-// const e = require("express")
-
-// import fetch from "node-fetch"
-
 document.getElementById('loginbtn').addEventListener('click',loginWithFacebook,false)
 
 function loginWithFacebook(){
-    // e.preventDefault()
     FB.login(response=>{
-        // console.log(response)
         const {authResponse:{accessToken,userID}}=response
-
         fetch('/login-with-facebook',{
             method: 'POST',
             headers: {
@@ -24,10 +17,7 @@ function loginWithFacebook(){
             console.log(jsonData);
             console.log(status);
             document.getElementById('myText').value =jsonData.data.accessToken;
-
-
           })
-        // console.log(ttt)
         FB.api('/me',function(response){
             // console.log(JSON.stringify(response))
         })
